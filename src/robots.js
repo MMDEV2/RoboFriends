b63e017ffd785f0ed3120ff9fabe81fbd1ba4fbb
Mike Murphy <58226575+MMDEV2@users.noreply.github.com>
2020-05-02 14:38:03 +0100

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Mike',
+    username: 'Mike',
+    email: 'mike.murphy@anouskahempeldesign.com'
+  },
+  {
+    id: 2,
+    name: 'Charles',
+    username: 'Charles',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Samuel',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Nevile',
+    username: 'Neville',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'LW',
+    username: 'LW',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Cindy',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Evelyn',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Barbara',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Russell',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Hugh',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
